4b029194e4171032e65af53341a1a71c1e3c80f3
Tom Anderson <portlester.tom@gmail.com>
2017-08-11 14:29:22 +1000

    Initial files

---
+++ main.js
@@ -0,0 +1,10 @@
+function activateSlide(slideNum) {
+     var slide = document.getElementById("slide" + slideNum);
+}
+
+
+document.onkeydown = function(event) {
+     if (event.key == "ArrowRight") {
+
+     }
+}
